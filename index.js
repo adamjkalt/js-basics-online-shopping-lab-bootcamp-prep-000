905490f5163cafe896e0f2db7d3274a3e159f644
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-14 03:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,33 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+let itemPrice = Math.floor(Math.random() * 101);
+var itemName = {};
+itemName[item] = itemPrice;
+cart.push(itemName);
+console.log(`${item} has been added to your cart.`);
+return cart;
 }
 
 function viewCart() {
-  // write your code here
+  console.log(cart);
+  if (cart.length === 0) {
+    console.log(`Your shopping cart is empty.`);
+  }
+  for (let i = 0; i < cart.length; i++) {
+    
+  if (cart.length > 0) {
+    console.log(`In your cart, you have ${Object.keys(cart[i])[0]} at $${cart[i][Object.keys(cart[i])[0]]}.`);
+  }
+  else if (cart.length > 1) {
+    console.log(`In your cart, you have ${Object.keys(cart[i])[0]} at $${cart[i][Object.keys(cart[i])[0]]} and ${Object.keys(cart[i])[1]} at $${cart[i][Object.keys(cart[i])[1]]}.`);
+  }
+  //while (cart.length > 2) {
+   // i++;
+   if (cart.length > 2) {
+    console.log(`In your cart, you have ${Object.keys(cart[i])[0]} at $${cart[i][Object.keys(cart[i])[0]]}, ${Object.keys(cart[i])[1]} at $${cart[i][Object.keys(cart[i])[1]]}, and ${Object.keys(cart[i])[2]} at $${cart[i][Object.keys(cart[i])[2]]} `);
+   }
+}
 }
 
 function total() {
