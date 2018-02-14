a6da32c126b003ebbbb0ae51f0732ef614de060b
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-14 05:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ function removeFromCart(item) {
 for (let i=0; i < cart.length; i++) {
 if (cart[i].hasOwnProperty(item)) {
      cart.splice(cart[i]);
+return cart;
   }
 }
 console.log("That item is not in your cart.");
