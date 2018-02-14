e591d6f25bc68b70633072db03f123bce3481bdf
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-14 04:38:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ return totalValue;
 function removeFromCart(item) {
 for (var i in cart) {
 var itemKey = Object.keys(cart[i])[0];
-cart.splice(itemKey());
+cart.splice(item());
 }
 return cart;
 }
