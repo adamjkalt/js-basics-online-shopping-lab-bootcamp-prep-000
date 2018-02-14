f92272669c8b69f5b1e5622207e9db53e3ee0af2
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-14 03:41:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,10 +43,11 @@ else if (currentCart.length > 2) {
 
 
 function total() {
+let currentCart = [];
 for (var i in cart) {
       var itemKey = Object.keys(cart[i])[0];
       var itemValue = cart[i][itemKey];
-console.log(itemKey.length);
+      currentCart.push(`${itemKey} at \$${itemValue}`);
 }
 }
 
