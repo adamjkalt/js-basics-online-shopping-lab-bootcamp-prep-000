608b0ca4fd008abbbc9da5b08a25a27a90fcc881
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-14 03:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ else if (currentCart.length > 2) {
 
 function getSum(final, num) {
     return final + num;
+}
 function total() {
 let currentCart = [];
 for (var i in cart) {
@@ -52,7 +53,7 @@ for (var i in cart) {
 document.getElementById(currentCart).innerHTML = currentCart.reduce(getSum);
 }
 }
-}
+
 
 function removeFromCart(item) {
   // write your code here
